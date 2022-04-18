ea029687d0caf2f268179648170e28e77a07fb9d
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-04-18 07:53:04 +0300

    Task 10 solution

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,45 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputNumber: document.querySelector('input'),
+  buttonCreate: document.querySelector('[data-create]'),
+  buttonDestroy: document.querySelector('[data-destroy]'),
+  newBoxes: document.querySelector('#boxes'),
+}
+
+// console.log(refs.inputNumber);
+// console.log(refs.buttonCreate);
+// console.log(refs.buttonDestroy);
+// console.log(refs.newBoxes);
+
+refs.buttonCreate.addEventListener('click', createBoxes);
+refs.buttonDestroy.addEventListener('click', destroyBoxes);
+
+let boxSize = 20;
+let boxSizeStep = 10;
+
+function createBoxes (amount) {
+  amount = refs.inputNumber.value;
+  const boxes = [];
+  
+  while (boxes.length < amount) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.height = `${boxSize + boxSizeStep}px`;
+    box.style.width = `${boxSize + boxSizeStep}px`; 
+    
+    boxSizeStep += 10;
+
+    boxes.push(box);
+  }
+
+  refs.newBoxes.append(...boxes);  
+}
+
+function destroyBoxes() {
+  refs.newBoxes.innerHTML = '';
+  refs.inputNumber.value = '';
+  boxSizeStep = 10;
+}
