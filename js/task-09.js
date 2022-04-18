9102970500893e41561782e1dc4186718975f2ec
Oleksandr Domanskiy <domanskyioleksandr@gmail.com>
2022-04-18 05:50:47 +0300

    Task 9 solution

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  colorEl: document.querySelector('.color'),
+  changeColorEl: document.querySelector('.change-color'),
+}
+
+refs.changeColorEl.addEventListener('click', onClickChangeButtonColor);
+
+function onClickChangeButtonColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.colorEl.textContent = getRandomHexColor();
+}
